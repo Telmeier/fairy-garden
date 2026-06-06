5fa34e778dc59341e2487c92018cf2491ea29026
Telmeier <timothyelmeier@gmail.com>
2026-06-06 01:05:44 -0400

    Update sketch_jun5a.ino

--- sketch_jun5a.ino
+++ sketch_jun5a.ino
@@ -2,9 +2,9 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <HTTPUpdate.h>
+#include <time.h>
 
-// --- Globals ---
-const int CURRENT_VERSION = 1; 
+const int CURRENT_VERSION = 2;
 const char* ssid = "Elmeier";
 const char* password = "7178246672";
 const char* gistUrl = "https://gist.github.com/Telmeier/c8299898f0cd224551bad47cf1787b1f/raw/config.json";
@@ -15,51 +15,59 @@ void setup() {
   Serial.begin(115200);
   for (int p : ledPins) pinMode(p, OUTPUT);
 
-  // Default values
-  int duration = 8;
-  int brightness = 75;
-  String hibernateUntil = "0";
-
   if (connectWiFi()) {
-    JsonDocument doc; 
+    configTime(-18000, 3600, "pool.ntp.org"); // Set to EST
+    JsonDocument doc;
     if (fetchConfig(doc)) {
-      // 1. Capture ALL your Gist settings
-      duration = doc["light_duration_hours"] | 8;
-      brightness = doc["brightness_percent"] | 75;
-      hibernateUntil = doc["unit_hibernate_until"].as<String>();
-
-      // 2. Check for Firmware Update
-      int remoteVersion = doc["firmware_version"] | 1;
-      if (remoteVersion > CURRENT_VERSION) {
+      
+      // OTA Update Check
+      if ((int)doc["firmware_version"] > CURRENT_VERSION) {
         httpUpdate.update(client, doc["ota_bin_url"]);
+      }
+
+      // Time Logic
+      struct tm timeinfo;
+      if (getLocalTime(&timeinfo)) {
+        String startTime = doc["start_time_12hr"].as<String>();
+        int targetHour = parseHour(startTime);
+        int targetMin = parseMinute(startTime);
+
+        // If current time is before start time, sleep until it is time
+        if (timeinfo.tm_hour < targetHour || (timeinfo.tm_hour == targetHour && timeinfo.tm_min < targetMin)) {
+          long sleepSecs = ((targetHour - timeinfo.tm_hour) * 3600) + ((targetMin - timeinfo.tm_min) * 60);
+          esp_sleep_enable_timer_wakeup(sleepSecs * 1000000ULL);
+          esp_deep_sleep_start();
+        }
+      }
+
+      // Flicker Routine
+      int duration = doc["light_duration_hours"] | 8;
+      int brightness = doc["brightness_percent"] | 100;
+      unsigned long endTime = millis() + ((unsigned long)duration * 3600000UL);
+      
+      while (millis() < endTime) {
+        for (int i = 0; i < 3; i++) {
+          analogWrite(ledPins[i], random(50, 255) * (brightness / 100.0));
+        }
+        delay(random(50, 150));
       }
     }
     WiFi.disconnect(true);
-    WiFi.mode(WIFI_OFF);
   }
-
-  // 3. Logic for Hibernation
-  if (hibernateUntil != "0") {
-    // Here you would add logic to check if current date/time is past hibernateUntil
-    // For now, it just bypasses the LEDs if not "0"
-    esp_deep_sleep_start(); 
-  }
-
-  // 4. Flicker with brightness factor
-  unsigned long endTime = millis() + ((unsigned long)duration * 3600000UL);
-  while (millis() < endTime) {
-    for (int i = 0; i < 3; i++) {
-      // Scale brightness by the Gist value (0.0 to 1.0)
-      float bFactor = brightness / 100.0;
-      analogWrite(ledPins[i], random(50, 255) * bFactor);
-    }
-    delay(random(50, 150));
-  }
-
-  // 5. Sleep
-  uint64_t sleepTimeSeconds = (24 - duration) * 3600ULL;
-  esp_sleep_enable_timer_wakeup(sleepTimeSeconds * 1000000ULL);
+  
+  // Sleep for remaining 24hr cycle
   esp_deep_sleep_start();
+}
+
+int parseHour(String t) {
+  int h = t.substring(0, t.indexOf(':')).toInt();
+  if (t.indexOf("pm") != -1 && h < 12) h += 12;
+  if (t.indexOf("am") != -1 && h == 12) h = 0;
+  return h;
+}
+
+int parseMinute(String t) {
+  return t.substring(t.indexOf(':') + 1, t.indexOf(':') + 3).toInt();
 }
 
 bool connectWiFi() {
@@ -74,13 +82,11 @@ bool connectWiFi() {
 bool fetchConfig(JsonDocument& doc) {
   HTTPClient http;
   http.begin(client, gistUrl);
-  int httpCode = http.GET();
-  if (httpCode == 200) {
+  if (http.GET() == 200) {
     deserializeJson(doc, http.getString());
     http.end();
     return true;
   }
-  http.end();
   return false;
 }
 
